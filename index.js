3b64c0a5393239cc6931058c50bc1de234c62c86
lizbur10 <liz.burton147@gmail.com>
2020-01-29 12:26:55 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const win = arr.find(record => record.result === 'W');
+  if (win) {
+    return win.year
+  }
+}
